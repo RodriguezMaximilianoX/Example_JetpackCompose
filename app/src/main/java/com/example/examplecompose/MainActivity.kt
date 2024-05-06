0a71eda349959c0b2e5467326426b1a5c6a6cef0
rodriguezmaximilianoxavier <rodriguezmaximilianoxavier@gmail.com>
2024-05-06 17:47:28 -0300

    add Google Maps

--- app/src/main/java/com/example/examplecompose/MainActivity.kt
+++ app/src/main/java/com/example/examplecompose/MainActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -40,10 +39,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.rememberMarkerState
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,20 +74,35 @@ fun ViewContainer() {
 
 @Composable
 fun MyGoogleMaps() {
-    val markerPosition = remember { mutableStateOf(LatLng(37.4219999, -122.0840575)) }
 
-    GoogleMap(modifier = Modifier.fillMaxSize()) {
+    val marker = LatLng(-31.449142127185834, -64.17518237640897)
+    val markerState = MarkerState(position = marker)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(marker, 10f)
+    }
+    val myProperties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.HYBRID))
+    }
+    val myUiSettings by remember {
+        mutableStateOf(MapUiSettings(zoomControlsEnabled = false))
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        properties = myProperties,
+        uiSettings = myUiSettings
+    ) {
         Marker(
-            state = rememberMarkerState(position = markerPosition.value),
-            title = "Mi Marcador"
-        ) {
-            // Aquí puedes personalizar el marcador, por ejemplo, agregar un icono
-        }
+            state = markerState,
+            title = "Cancha Taiere",
+            snippet = "Marker in Taiere"
+        )
     }
 }
 
 @Composable
-fun FAB(){
+fun FAB() {
     val context = LocalContext.current
     FloatingActionButton(onClick = {
         Toast.makeText(context, "Pressed button", Toast.LENGTH_SHORT).show()
